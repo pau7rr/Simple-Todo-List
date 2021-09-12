4b3d4c9ba40c2b5560400547410916603c62a5e5
pau7rr <pau7rr@gmail.com>
2021-09-12 23:27:34 +0200

    Base project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <todo-list></todo-list>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat');
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-family: Montserrat;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,101 @@
+<template lang="es">
+<div class="task-list">
+    <form class="form" @submit.prevent="createTasks">
+        <label class="label" for="task">New task: </label>
+        <input class="input" type="text" v-model="newTask" id="task"> 
+        <input class="button" type="submit" value="Create task">
+    </form>
+        <ul class="list">
+            <li 
+            class="task"
+            v-for="(task, i) in tasks" 
+            :key="'task' + i"
+            :class="{completed: task.completed}"
+            @click="completeTask(task.id)"
+            >
+            {{task.text}} 
+            <label class="trash" @click="deleteTask(task.id)">🗑️</label>
+            </li>
+            
+        </ul>
+    </div>
+</template>
+<script>
+let counter = 0;
+export default {
+    data: () => ({
+        newTask: "",
+        tasks: [],
+    }),
+    methods: {
+        createTasks(){
+            let task = {
+                id: counter++,
+                text: this.newTask,
+                completed: false,
+            }
+            this.tasks.push(task),
+            this.newTask = ""
+        },
+        completeTask(id){
+            let findTask = this.tasks.find( obj => {
+                return obj.id === id
+            })
+            if (findTask.completed) return findTask.completed = false
+                
+            findTask.completed = true
+        },
+        deleteTask(id){
+            let findTask = this.tasks.find( obj => {
+                return obj.id === id
+            })
+            this.tasks.splice(findTask)
+        }
+    }
+}
+</script>
+<style scoped>
+    .task-list {
+        width: 800px;
+        max-width: 100%;
+        margin: 0px auto;
+    }
+    .form {
+        background: white;
+        border-radius: 12px;
+        padding: 30px;
+        box-shadow: 0px 10px 22px -1px rgba(0,0,0,0.25);
+        margin-top: 10px;
+    }
+    .label {
+        display: block;
+        margin-bottom: 10px;
+    }
+    .input {
+        height: 35px;
+    }
+    .button {
+        margin-left: 20px;
+        height: 35px;
+        border: none;
+        border-radius: 5px;
+        box-shadow: 0 1px 4px rgba(0, 0, 0, .2);
+        background-color: darkred;
+        color: #ecf0f1;
+        cursor: pointer
+    }
+    .list {
+        margin-top: 40px;
+    }
+    .task {
+        cursor: pointer;
+        margin: 10px 0;
+    }
+    .completed {
+        color: darkred;
+        text-decoration: line-through;
+    }
+    .trash {
+        margin-left: 20px;
+    }
+</style>
